e480828d2a8fe648bc044a9e913877e92edc0bf7
minakshitomar <tomar.minakshi@outlook.com>
2022-12-23 18:24:22 +0530

    linked list

--- day12.c
+++ day12.c
@@ -108,49 +108,6 @@ struct node* delete(int key)
    return current;
 }
 
-// void sort() 
-// {
-//    int i, j, k, tempKey, tempData;
-//    struct node *current;
-//    struct node *next;	
-//    int size = length();
-//    k = size ;
-//    for ( i = 0 ; i < size - 1 ; i++, k-- ) 
-//    {
-//       current = head;
-//       next = head->next;	
-//       for ( j = 1 ; j < k ; j++ ) 
-//       {   
-//          if ( current->data > next->data ) 
-//          {
-//             tempData = current->data;
-//             current->data = next->data;
-//             next->data = tempData;
-//             tempKey = current->key;
-//             current->key = next->key;
-//             next->key = tempKey;
-//          }
-//          current = current->next;
-//          next = next->next;
-//       }
-//    }   
-// }
-
-// void reverse(struct node** head_ref) 
-// {
-//    struct node* prev   = NULL;
-//    struct node* current = *head_ref;
-//    struct node* next;	
-//    while (current != NULL) 
-//    {
-//       next  = current->next;
-//       current->next = prev;   
-//       prev = current;
-//       current = next;
-//    }
-//    *head_ref = prev;
-// }
-
 void main() 
 {
    insertFirst(1,10);
@@ -211,10 +168,4 @@ void main()
       printf("Element not found.");
    }
    printf("\n");
-//    sort();
-//    printf("List after sorting the data: ");
-//    printList();
-//    reverse(&head);
-//    printf("\nList after reversing the data: ");
-//    printList();
 }
